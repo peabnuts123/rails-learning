ce0c595ae059cf322dde7a99e987fbb199302c99
Jeff Andrews <jeff.andrews@alphero.com>
2020-05-11 10:01:28 +1200

    Add Comments resource, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root action: :index, controller: 'welcome'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
