411c91ecce402ebd3865017f58bc1061c9e40fed
Jeff Andrews <jeff.andrews@alphero.com>
2020-05-11 17:01:18 +1200

    Add CRUD operations for article comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+  def create
+    puts "Create comment"
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    if @comment
+      redirect_to article_path(@article)
+    elsif
+      render 'new'
+    end
+  end
+
+  def destroy
+    puts "Destroy comment"
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private
+  def comment_params
+    return params.require(:comment).permit(
+      :commenter,
+      :body,
+    )
+  end
 end
